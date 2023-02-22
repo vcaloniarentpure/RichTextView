c8a31991107f263226b86d7b4c71d6e4dcb4365f
viktoricalonia <viktor.calonia@myahmed.com>
2023-02-22 12:44:42 +0800

    Revert init method for htmlstyle params

--- Source/HTML Rendering/HTMLStyleParams.swift
+++ Source/HTML Rendering/HTMLStyleParams.swift
@@ -13,4 +13,12 @@ public struct HTMLStyleParams: Hashable {
     let h2Font: UIFont
     let h3Font: UIFont
     let boldFont: UIFont
+
+    public init(baseFont: UIFont, h1Font: UIFont, h2Font: UIFont, h3Font: UIFont, boldFont: UIFont) {
+        self.baseFont = baseFont
+        self.h1Font = h1Font
+        self.h2Font = h2Font
+        self.h3Font = h3Font
+        self.boldFont = boldFont
+    }
 }
